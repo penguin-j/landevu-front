37aa171a99278be60fa7a646081f8d1fa119b09e
j <penguinjinbei@gmail.com>
2022-11-26 23:20:25 +0900

    feat: mod RecommendAreas

---
+++ pages/areas/recommend.tsx
@@ -0,0 +1,5 @@
+const RecommendAreas = () => {
+  return <h1>input departure spots</h1>
+}
+
+export default RecommendAreas
--- pages/areas/recommend/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function RecommendArea() {
-  return <h1>input departure spots</h1>
-}
