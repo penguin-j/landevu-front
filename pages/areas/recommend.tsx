29326a31d97784d149598b5beb0900bbadea76b8
j <penguinjinbei@gmail.com>
2022-11-27 17:20:12 +0900

    feat: add Client-side data fetching

--- pages/areas/recommend.tsx
+++ pages/areas/recommend.tsx
@@ -1,5 +1,20 @@
+import useSWR from 'swr'
+
+const fetcher = async (url) => await fetch(url).then((res) => res.json())
+
 const RecommendAreas = () => {
-  return <h1>input departure spots</h1>
+  const { data, error } = useSWR('https://api.github.com/repos/vercel/next.js', fetcher)
+
+  if (error) return <>failed to load</>
+  if (!data) return <>loading...</>
+
+  return (
+    <>
+      <pre>
+        <object>{JSON.stringify(data)}</object>
+      </pre>
+    </>
+  )
 }
 
 export default RecommendAreas
