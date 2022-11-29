e9831bdc18122c6e3c54e3b97c8bd5d6f43d6bc1
j <penguinjinbei@gmail.com>
2022-11-30 00:38:07 +0900

    feat: show search form before request, and show result after request

--- pages/areas/recommend.tsx
+++ pages/areas/recommend.tsx
@@ -7,14 +7,23 @@ const fetcher = async (url) => await fetch(url).then((res) => res.json())
 const RecommendAreas = () => {
   const router = useRouter()
 
-  const { data, error } = useSWR(`https://api.github.com/repos/${router.query.input}`, fetcher)
+  const input = router.query.input
+
+  if (!input) {
+    return (
+      <>
+        <Search submit={() => {}} />
+      </>
+    )
+  }
+
+  const { data, error } = useSWR(`https://api.github.com/repos/${input}`, fetcher)
 
   if (error) return <>failed to load</>
   if (!data) return <>loading...</>
 
   return (
     <>
-      <Search submit={() => {}} />
       <pre>
         <object>{JSON.stringify(data)}</object>
       </pre>
