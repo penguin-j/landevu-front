65ca8d8cae548b3d19267a37447f44d4f4899ae3
j <penguinjinbei@gmail.com>
2022-11-29 22:41:17 +0900

    feat: show search result on /areas/recommend

--- pages/areas/recommend.tsx
+++ pages/areas/recommend.tsx
@@ -1,15 +1,20 @@
+import { useRouter } from 'next/router'
 import useSWR from 'swr'
+import { Search } from '../components/search'
 
 const fetcher = async (url) => await fetch(url).then((res) => res.json())
 
 const RecommendAreas = () => {
-  const { data, error } = useSWR('https://api.github.com/repos/vercel/next.js', fetcher)
+  const router = useRouter()
+
+  const { data, error } = useSWR(`https://api.github.com/repos/${router.query.input}`, fetcher)
 
   if (error) return <>failed to load</>
   if (!data) return <>loading...</>
 
   return (
     <>
+      <Search submit={() => {}} />
       <pre>
         <object>{JSON.stringify(data)}</object>
       </pre>
